de8308c92e0d1153f3a5a5ac5e1a7ebe91fbfb62
tanikshapatil-coditas <taniksha.patil@coditas.com>
2024-12-17 22:50:42 +0530

    Initial commit

---
+++ trainix/src/main/java/com/example/trainix/TrainixApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainixApplication.class, args);
+	}
+
+}
---
+++ trainix/src/test/java/com/example/trainix/TrainixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
